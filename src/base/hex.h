c05244972c797dff38071f4817140b9b4ddfa7e4
Руслан Карабалин <r.karabalin@avsw.ru>
2022-08-03 18:13:42 +0300

    Hex refactoring

--- src/base/hex.h
+++ src/base/hex.h
@@ -12,35 +12,47 @@
 #include <stdexcept>
 #include <utility>
 
+namespace rev {
 inline constexpr long double PI{3.14159265358979323846};
 
-namespace rev {
 template <class T>
 struct Hex {
  public:
   using crd_t = T;
-  Hex(crd_t, crd_t);
+
+ public:
+  constexpr explicit Hex(crd_t, crd_t) noexcept;
   Hex(crd_t, crd_t, crd_t);
   Hex(const Hex& rhs) noexcept;
   Hex(Hex&& rhs) noexcept;
+
   crd_t q() const noexcept { return q_; }
   crd_t r() const noexcept { return r_; }
   crd_t s() const noexcept { return -q_ - r_; }
-  static crd_t distance(const Hex& lhs, const Hex& rhs) noexcept;
-  static const Hex& direction(int dir);
-  Hex neighbor(int dir) const;
+
   Point<crd_t> to_pixel(const Layout& layout) const noexcept;
-  static Point<crd_t> corner_offset(const Layout& layout, int corner);
-  std::array<Point<crd_t>, 6> polygon_corners(const Layout& layout) const;
+
+  std::array<Point<crd_t>, 6> polygon_corners(
+      const Layout& layout) const noexcept;
+
+  Hex neighbor(int dir) const;
+
+ public:
+  static const Hex& direction(int dir);
+
+  static crd_t distance(const Hex& lhs, const Hex& rhs) noexcept;
+
+  static Point<crd_t> corner_offset(const Layout& layout, int corner) noexcept;
+
   static Hex<crd_t> from_pixel(const Layout& layout,
-                               const Point<crd_t>& p) = delete;
+                               const Point<crd_t>& p) noexcept = delete;
 
  private:
   const crd_t q_, r_;
 };
 
 template <class T>
-Hex<T>::Hex(crd_t q, crd_t r) : q_(q), r_(r) {}
+constexpr Hex<T>::Hex(crd_t q, crd_t r) noexcept : q_(q), r_(r) {}
 
 template <class T>
 Hex<T>::Hex(crd_t q, crd_t r, crd_t s) : q_(q), r_(r) {
@@ -59,39 +71,35 @@ Hex<T>::Hex(Hex&& rhs) noexcept
 template <class T>
 typename Hex<T>::crd_t Hex<T>::distance(const Hex<T>& lhs,
                                         const Hex<T>& rhs) noexcept {
-  return std::abs(lhs.q() / 2 - rhs.q() / 2) +
-         std::abs(lhs.r() / 2 - rhs.r() / 2) +
-         std::abs(lhs.s() / 2 - rhs.s() / 2);
+  return std::max(std::abs(lhs.q() - rhs.q()) + std::abs(lhs.r() - rhs.r()) +
+                  std::abs(lhs.s() - rhs.s()));
 }
 
 template <class T>
 Point<typename Hex<T>::crd_t> Hex<T>::to_pixel(
     const Layout& layout) const noexcept {
-  auto x = (layout.orientation.f0 * q() + layout.orientation.f1 * r()) *
-           layout.size.x;
-  auto y = (layout.orientation.f2 * q() + layout.orientation.f3 * r()) *
-           layout.size.y;
+  auto x{(layout.orientation.f0 * q() + layout.orientation.f1 * r()) *
+         layout.size.x};
+  auto y{(layout.orientation.f2 * q() + layout.orientation.f3 * r()) *
+         layout.size.y};
   return {static_cast<crd_t>(x + layout.origin.x),
           static_cast<crd_t>(y + layout.origin.y)};
 }
 
 template <class T>
-std::array<Point<T>, 6> Hex<T>::polygon_corners(const Layout& layout) const {
-  Point<crd_t> center = this->to_pixel(layout);
-  std::array<Point<crd_t>, 6> corners = {
-      center + corner_offset(layout, 0), center + corner_offset(layout, 1),
-      center + corner_offset(layout, 2), center + corner_offset(layout, 3),
-      center + corner_offset(layout, 4), center + corner_offset(layout, 5),
-  };
-  return corners;
+std::array<Point<T>, 6> Hex<T>::polygon_corners(
+    const Layout& layout) const noexcept {
+  Point<crd_t> center{this->to_pixel(layout)};
+  return {center + corner_offset(layout, 0), center + corner_offset(layout, 1),
+          center + corner_offset(layout, 2), center + corner_offset(layout, 3),
+          center + corner_offset(layout, 4), center + corner_offset(layout, 5)};
 }
 
 template <class T>
 const Hex<T>& Hex<T>::direction(int dir) {
   assert(dir >= 0 && dir < 6);
-  static std::array<Hex, 6> dirs = {Hex(1, 0, -1), Hex(1, -1, 0),
-                                    Hex(0, -1, 1), Hex(-1, 0, 1),
-                                    Hex(-1, 1, 0), Hex(0, 1, -1)};
+  static Hex dirs[6]{Hex{1, 0, -1}, Hex{1, -1, 0}, Hex{0, -1, 1},
+                     Hex{-1, 0, 1}, Hex{-1, 1, 0}, Hex{0, 1, -1}};
   return dirs[dir];
 }
 
@@ -102,56 +110,61 @@ Hex<T> Hex<T>::neighbor(int dir) const {
 
 template <class T>
 struct HexHash {
-  std::size_t operator()(const Hex<T>& h) const {
+  std::size_t operator()(const Hex<T>& h) const noexcept {
     return std::hash<Hex<T>::crd_t>()(h.q()) << 2 ^
            std::hash<Hex<T>::crd_t>()(h.r()) << 1 ^
            std::hash<Hex<T>::crd_t>()(h.s());
   }
 };
 
-inline Hex<double> from_pixel(const Layout& layout, const Point<int>& p) {
-  double ptx = static_cast<double>(p.x - layout.origin.x) / layout.size.x;
-  double pty = static_cast<double>(p.y - layout.origin.y) / layout.size.y;
-  return {static_cast<double>(layout.orientation.b0 * ptx +
-                              layout.orientation.b1 * pty),
-          static_cast<double>(layout.orientation.b2 * ptx +
-                              layout.orientation.b3 * pty)};
+inline Hex<double> from_pixel(const Layout& layout,
+                              const Point<int>& p) noexcept {
+  auto ptx{static_cast<double>(static_cast<uint64_t>(p.x) - layout.origin.x) /
+           layout.size.x};
+  auto pty{static_cast<double>(static_cast<uint64_t>(p.y) - layout.origin.y) /
+           layout.size.y};
+  return Hex{static_cast<double>(layout.orientation.b0 * ptx +
+                                 layout.orientation.b1 * pty),
+             static_cast<double>(layout.orientation.b2 * ptx +
+                                 layout.orientation.b3 * pty)};
 }
 
 inline Hex<int> round_cube(Hex<double> hex) {
-  int q2 = static_cast<int>(std::lround(hex.q()));
-  int r2 = static_cast<int>(std::lround(hex.r()));
-  int s2 = static_cast<int>(std::lround(hex.s()));
-  double q_diff = std::abs(q2 - hex.q());
-  double r_diff = std::abs(r2 - hex.r());
-  double s_diff = std::abs(s2 - hex.s());
-  if (q_diff > r_diff && q_diff > s_diff)
+  auto q2{static_cast<int>(std::lround(hex.q()))};
+  auto r2{static_cast<int>(std::lround(hex.r()))};
+  auto s2{static_cast<int>(std::lround(hex.s()))};
+  double q_diff{std::abs(q2 - hex.q())};
+  double r_diff{std::abs(r2 - hex.r())};
+  double s_diff{std::abs(s2 - hex.s())};
+  if (q_diff > r_diff && q_diff > s_diff) {
     q2 = -r2 - s2;
-  else if (r_diff > s_diff)
+  } else if (r_diff > s_diff) {
     r2 = -q2 - s2;
-  else
+  } else {
     s2 = -q2 - r2;
+  }
   return {q2, r2, s2};
 }
 
 template <>
-Hex<int> Hex<int>::from_pixel(const Layout& layout, const Point<crd_t>& p) {
+Hex<int> Hex<int>::from_pixel(const Layout& layout,
+                              const Point<crd_t>& p) noexcept {
   return round_cube(rev::from_pixel(layout, p));
 }
 
 template <class T>
 Point<typename Hex<T>::crd_t> Hex<T>::corner_offset(const Layout& layout,
-                                                    int corner) {
-  auto angle = 2 * PI * (layout.orientation.start_angle + corner) / 6;
+                                                    int corner) noexcept {
+  auto angle{2 * PI * (layout.orientation.start_angle + corner) / 6};
   return {static_cast<crd_t>(layout.size.x * std::cos(angle)),
           static_cast<crd_t>(layout.size.y * std::sin(angle))};
 }
 
 template <class T>
 Hex<T> operator+(const Hex<T>& lhs, const Hex<T>& rhs) {
-  auto x = Nof(lhs.q()) + rhs.q();
-  auto y = Nof(lhs.r()) + rhs.r();
-  auto z = Nof(lhs.s()) + rhs.s();
+  auto x{Nof(lhs.q()) + rhs.q()};
+  auto y{Nof(lhs.r()) + rhs.r()};
+  auto z{Nof(lhs.s()) + rhs.s()};
   if (!x.has_value() || !y.has_value() || !z.has_value()) {
     BOOST_LOG_TRIVIAL(error)
         << "Hex coordinates overflow occured while executing "
@@ -167,9 +180,9 @@ Hex<T> operator+(const Hex<T>& lhs, const Hex<T>& rhs) {
 
 template <class T>
 Hex<T> operator-(const Hex<T>& lhs, const Hex<T>& rhs) {
-  auto x = Nof(lhs.q()) - rhs.q();
-  auto y = Nof(lhs.r()) - rhs.r();
-  auto z = Nof(lhs.s()) - rhs.s();
+  auto x{Nof(lhs.q()) - rhs.q()};
+  auto y{Nof(lhs.r()) - rhs.r()};
+  auto z{Nof(lhs.s()) - rhs.s()};
   if (!x.has_value() || !y.has_value() || !z.has_value()) {
     BOOST_LOG_TRIVIAL(error)
         << "Hex coordinates overflow occured while executing "
@@ -180,14 +193,14 @@ Hex<T> operator-(const Hex<T>& lhs, const Hex<T>& rhs) {
         << rhs.q() << ';' << rhs.r() << ';' << rhs.s() << ")\n";
     return lhs;
   }
-  return Hex(x.get(), y.get(), z.get());
+  return Hex{x.get(), y.get(), z.get()};
 }
 
 template <class T>
 Hex<T> operator*(const Hex<T>& lhs, const Hex<T>& rhs) {
-  auto x = Nof(lhs.q()) * rhs.q();
-  auto y = Nof(lhs.r()) * rhs.r();
-  auto z = Nof(lhs.s()) * rhs.s();
+  auto x{Nof(lhs.q()) * rhs.q()};
+  auto y{Nof(lhs.r()) * rhs.r()};
+  auto z{Nof(lhs.s()) * rhs.s()};
   if (!x.has_value() || !y.has_value() || !z.has_value()) {
     BOOST_LOG_TRIVIAL(error)
         << "Hex coordinates overflow occured while executing "
@@ -198,7 +211,7 @@ Hex<T> operator*(const Hex<T>& lhs, const Hex<T>& rhs) {
         << rhs.q() << ';' << rhs.r() << ';' << rhs.s() << ")\n";
     return lhs;
   }
-  return Hex(x.get(), y.get(), z.get());
+  return Hex{x.get(), y.get(), z.get()};
 }
 
 template <class T>
