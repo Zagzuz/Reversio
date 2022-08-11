9f00e0ef2655cbf5b9e4dd8506a0ef3faff525c5
Ruslan <heckfynook@gmail.com>
2022-08-11 22:47:33 +0300

    Remove rev::Point old impl

--- src/base/point.h
+++ src/base/point.h
@@ -1,28 +1,13 @@
+#ifndef POINT_H
+#define POINT_H 
+
 #include <SFML/System/Vector2.hpp>
 
-#ifndef POINT_H
-#define POINT_H
-
 namespace rev {
-/*template <typename T>
-struct Point
-{
-        constexpr Point(T x, T y) noexcept : x(x), y(y) {}
-        Point(const Point& p) noexcept : x(p.x), y(p.y) {}
-        Point& operator=(Point p) noexcept { std::swap(*this, p); return *this;
-} const T x, y;
-};
-
-template <typename T>
-Point<T> operator+(const Point<T>& lhs, const Point<T>& rhs) noexcept
-{
-        return { lhs.x + rhs.x, lhs.y + rhs.y };
-}*/
-
 template <typename T>
 using Point = sf::Vector2<T>;
 
-using IntPt = sf::Vector2i;
+using IntPt = sf::Vector2<int>;
 using FracPt = sf::Vector2<double>;
 }  // namespace rev
 
